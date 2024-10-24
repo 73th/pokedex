0d5da864aa8b1a9a5893758426b2a36ae3a7fa5e
theo <boronattheo83@gmail.com>
2024-10-24 09:28:43 +0200

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
-
 import PokemonCard from "./components/PokemonCard";
 
-<div>
-	<PokemonCard />
-</div>;
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
 
-export default PokemonCard;
+function App() {
+	return <PokemonCard pokemon={pokemonList[0]} />;
+}
+
+export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,9 @@
-function PokemonCard() {
-	const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
 	return (
 		<>
 			<figure>
 				{pokemon.imgSrc ? (
-					<img src={pokemon.imgSrc} alt="Bulbizarre" />
+					<img src={pokemon.imgSrc} alt={pokemon.name} />
 				) : (
 					<p>???</p>
 				)}
@@ -14,16 +12,5 @@ function PokemonCard() {
 		</>
 	);
 }
-
-const pokemonList = [
-	{
-		name: "bulbasaur",
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-	{
-		name: "mew",
-	},
-];
 
 export default PokemonCard;
